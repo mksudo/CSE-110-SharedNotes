490db6ebf5a7b9e1df148284b615e1b31f4f2cc8
mksudo <mkzhanghl@gmail.com>
2023-03-01 17:39:34 -0800

    impl note server api

--- app/src/main/java/edu/ucsd/cse110/sharednotes/model/NoteAPI.java
+++ app/src/main/java/edu/ucsd/cse110/sharednotes/model/NoteAPI.java
@@ -4,17 +4,17 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 
 public class NoteAPI {
-    // TODO: Implement the API using OkHttp!
-    // TODO: Read the docs: https://square.github.io/okhttp/
-    // TODO: Read the docs: https://sharednotes.goto.ucsd.edu/docs
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
     private volatile static NoteAPI instance = null;
 
-    private OkHttpClient client;
+    private final OkHttpClient client;
 
     public NoteAPI() {
         this.client = new OkHttpClient();
@@ -47,6 +47,46 @@ public class NoteAPI {
             Log.i("ECHO", body);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public Note getNote(String title) {
+        String encodedTitle = title.replace(" ", "%20");
+
+        var request = new Request.Builder()
+                .url("https://sharednotes.goto.ucsd.edu/notes/" + encodedTitle)
+                .get()
+                .build();
+
+        try (var response = client.newCall(request).execute()) {
+            assert response.body() != null;
+            var body = response.body().string();
+            Log.i(NoteAPI.class.getName(), "get note with title [" + title + "] => " + body);
+            return Note.fromJSON(body);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    public void putNote(Note note) {
+        String encodedTitle = note.title.replace(" ", "%20");
+
+        String noteJson = new Gson().toJson(note);
+
+        var requestBody = RequestBody.create(noteJson, JSON);
+
+        var request = new Request.Builder()
+                .url("https://sharednotes.goto.ucsd.edu/notes/" + encodedTitle)
+                .put(requestBody)
+                .build();
+
+        try (var response = client.newCall(request).execute()) {
+            assert response.body() != null;
+            var body = response.body().string();
+            Log.i(NoteAPI.class.getName(), "put note " + noteJson + "; response is " + body);
+        } catch (Exception exception) {
+            exception.printStackTrace();
         }
     }
 }
